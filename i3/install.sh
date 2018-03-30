9fa0999f2e8b646644bfb6f116aeeac6eed9f0c1
Michal Seta <djiamnot@gmail.com>
2018-03-30 15:05:48 -0400

    add riot to install script

--- i3/install.sh
+++ i3/install.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 
 source_directory="$(cd $(dirname ${BASH_SOURCE[0]}) && pwd)"
+echo "Adding syncthing PPA to apt"
 curl -s https://syncthing.net/release-key.txt | sudo apt-key add -
+echo "Adding Riot PPA to apt"
+sudo sh -c "echo 'deb https://riot.im/packages/debian/ artful main' > /etc/apt/sources.list.d/matrix-riot-im.list"
+curl -L https://riot.im/packages/debian/repo-key.asc | sudo apt-key add -
+echo "Update apt cache"
 sudo apt update
 sudo apt install i3 i3-wm i3status i3blocks suckless-tools feh compton gtk-chtheme \
     qt4-qtconfig sysstat acpi git automake build-essential gtk-doc-tools \
     gobject-introspection xbacklight scrot kbdd xautolock pavucontrol fonts-font-awesome \
-    xscreensaver syncthing gsimplecal ssh-askpass
+    xscreensaver syncthing gsimplecal ssh-askpass riot-web
 
-
-
-
-pushd .
 # Install playerctl
 cd /tmp && rm -rf playerctl
 git clone https://github.com/acrisci/playerctl.git
@@ -25,5 +26,3 @@ git clone https://github.com/nonpop/xkblayout-state.git
 cd xkblayout-state
 make -j$(nproc)
 cp xkblayout-state ${source_directory}/
-
-popd
